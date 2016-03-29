09215cbfb2701049b4dc44d5bd47970da72eecee
camachowebworld <camachowebworld@yahoo.com>
2016-03-29 11:01:12 -0700

    started header layout

--- header.php
+++ header.php
@@ -24,27 +24,49 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'underscores' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+	<header>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+	  <div class="container container-top-level header--wrapper">
+	    <div class="header--logo">
+	      <a href="http://zimmyb.com"><img src="" alt="Zimmy Boooks! Logo" /></a>
+	    </div>
+	    <div class="header--right-content">
+				<ul class="nav navbar-nav">
+					<li><a href="#">Books</a></li>
+					<!-- <li><a href="#">Store</a></li> -->
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">About</a></li>
+				</ul>
+			</div>
+	  </div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'underscores' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	  <div class="container container-top-level navbar-wrapper">
+	    <nav class="navbar navbar-default">
+	      <div class="container-fluid">
+	        <!-- Mobile Toggle -->
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	        </div>
+	        <!-- Header Navigation -->
+	        <div class="collapse navbar-collapse">
+	          <ul class="nav navbar-nav">
+	            <li><a href="#">Books</a></li>
+	            <!-- <li><a href="#">Store</a></li> -->
+	            <li><a href="#">Blog</a></li>
+							<li><a href="#">News</a></li>
+	            <li><a href="#">About</a></li>
+	          </ul>
+	        </div>
+	      </div>
+	    </nav>
+	  </div>
+
+	</header>
 
 	<div id="content" class="site-content">
