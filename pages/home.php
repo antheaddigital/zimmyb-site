1085c35d3685d3814b4e4f427dd369fe702ae9d4
Anthony Camacho <anthony.g.camacho@mail.com>
2017-03-25 00:02:01 -0700

    working on homepage

--- pages/home.php
+++ pages/home.php
@@ -11,47 +11,45 @@
 
 <!-- site-intro -->
 <section class="site-intro">
-  <h1>Books and Games for Learning Sign Language - All AGES / Everybody!</h1>
-  <img class="zimmy-home img-responsive" src="" />
-  <div class="site-intro-description">
-    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+  <div class="intro-wrapper">
+    <div class="site-intro-description">
+      <h1>Books and Games for Learning Sign Language</h1>
+      <p>Welcome to Zimmy Books! Here you will learn sign lanuage in a very fun way. All learning material you find here at Zimmy Books is, and will always be, 100% free.<br />ALL AGES / EVERYBODY!</p>
+    </div>
+    <img class="zimmy-home img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/zimmy-book-sign-1.png" />
+    <div class="clear"></div>
   </div>
 </section>
 <!-- site-intro - end -->
 
-<!-- hero -->
-<section class="hero">
-  <div class="hero-left">
-    <a href="" class=""><img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=635&h=400" /></a>
-  </div>
-  <div class="hero-right">
-    <a href="" class=""><img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=635&h=200" /></a>
-    <a href="" class=""><img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=635&h=200" /></a>
-  </div>
-  <div class="clear"></div>
-</section>
-<!-- hero - end -->
-
 <!-- books -->
-<section class="recent-posts">
-  <h2></h2>
-  <div class="recent-post recent-posts-blog">
-    <img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=800&h=400" />
-    <h3>Most recent blog post title</h3>
-  </div>
-  <div class="recent-post recent-posts-news">
-    <img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=800&h=400" />
-    <h3>Most recent news post title</h3>
+<section class="books">
+  <h2>Family Sign Language Books</h2>
+  <div class="books-copy-wrapper">
+    <div class="books-description">
+      <h3>Planet Colors!</h2>
+      <p>Travel around the world and learn the colors that make each location a special place to visit.</p>
+      <a href="<?php echo esc_url( site_url() ); ?>/books/planet-colors">Read Now! ></a>
+    </div>
+    <a class="books-featured" href="<?php echo esc_url( site_url() ); ?>/books/planet-colors"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/home-book-featured.jpg" /></a>
+    <div class="clear"></div>
   </div>
 </section>
-<!-- books - end -->
-
-<!-- books -->
-<!-- <section class="books"></section> -->
 <!-- books - end -->
 
 <!-- games -->
-<!-- <section class="games"></section> -->
+<section class="games">
+  <h2>Sign Language Games for Everybody</h2>
+  <div class="games-copy-wrapper">
+    <a class="games-featured" href="<?php echo esc_url( site_url() ); ?>/games/save-the-pig"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/home-game-featured.jpg" /></a>
+    <div class="games-description">
+      <h3>Save the Pig!</h3>
+      <p>Can help Honkatonk Pig, Squiky Pig, War Pig, and Poopy Pig from getting their much needed baths? You will learn the alphebet in Sign Language and also how to type.</p>
+      <a href="<?php echo esc_url( site_url() ); ?>/games/save-the-pig">Play Now! ></a>
+    </div>
+    <div class="clear"></div>
+  </div>
+</section>
 <!-- games - end -->
 
 <?php
