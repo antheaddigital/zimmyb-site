d40f9e500cdac092dfad1d627e78a6acc2f62fd0
Anthony Camacho <anthony.g.camacho@mail.com>
2017-03-30 21:17:40 -0700

    cleaning up site

--- pages/home.php
+++ pages/home.php
@@ -14,7 +14,7 @@
   <div class="intro-wrapper">
     <div class="site-intro-description">
       <h1>Books and Games for Learning Sign Language</h1>
-      <p>Welcome to Zimmy Books! Here you will learn sign language in a very fun way. All learning material you find here at Zimmy Books is, and will always be, 100% free.<br />ALL AGES / EVERYBODY!</p>
+      <p>Welcome to Zimmy Books! Here you will learn sign language in a very fun way. All learning material you find here at Zimmy Books is, and will always be, 100% free.<span>ALL AGES / EVERYBODY!</span></p>
     </div>
     <img class="zimmy-home img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/zimmy-book-sign-1.png" />
     <div class="clear"></div>
@@ -28,32 +28,32 @@
   <h2>Featured Releases!</h2>
 
   <div class="featured-wrapper featured-left">
-    <div class="featured-description planet-colors">
+    <a class="featured-img featured-img-planet-colors" href="<?php echo esc_url( site_url() ); ?>/books/planet-colors"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/planet-colors-featured.jpg" /></a>
+    <div class="featured-description featured-description-planet-colors">
       <h3>Planet Colors!</h2>
       <p>Travel around the world and learn the colors that make each location a special place to visit.</p>
       <a class="read-play-btn-01" href="<?php echo esc_url( site_url() ); ?>/books/planet-colors">read!</a>
     </div>
-    <a class="featured-img" href="<?php echo esc_url( site_url() ); ?>/books/planet-colors"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/planet-colors-featured.jpg" /></a>
     <div class="clear"></div>
   </div>
 
   <div class="featured-wrapper featured-right">
-    <div class="featured-description bayme-pants-the-sun-its-broken">
+    <a class="featured-img featured-img-bayme-pants-the-sun-its-broken" href="<?php echo esc_url( site_url() ); ?>/games/save-the-pig"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/the-sun-its-broken-featured.jpg" /></a>
+    <div class="featured-description featured-description-bayme-pants-the-sun-its-broken">
       <h3>Bayme Pants - The Sun! It's Broken!</h3>
       <p>Hang out with Bayme Pants and friends as they try to find out why the sun is broken. The signs you will learn are: Sun, Moon, Fish, Monkey, Rabbit, and more.</p>
       <a class="read-play-btn-01" href="<?php echo esc_url( site_url() ); ?>/books/bayme-pants-the-sun-its-broken">read!</a>
     </div>
-    <a class="featured-img" href="<?php echo esc_url( site_url() ); ?>/games/save-the-pig"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/the-sun-its-broken-featured.jpg" /></a>
     <div class="clear"></div>
   </div>
 
   <div class="featured-wrapper featured-left">
-    <div class="featured-description save-the-pig">
+    <a class="featured-img featured-img-save-the-pig" href="<?php echo esc_url( site_url() ); ?>/games/save-the-pig"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/save-the-pig-featured.jpg" /></a>
+    <div class="featured-description featured-description-save-the-pig">
       <h3>Save the Pig!</h3>
       <p>Can you help Honkatonk Pig, Squiky Pig, War Pig, and Poopy Pig from getting their much needed baths? You will learn the alphebet in Sign Language and also how to type.</p>
       <a class="read-play-btn-01" href="<?php echo esc_url( site_url() ); ?>/games/save-the-pig">play!</a>
     </div>
-    <a class="featured-img" href="<?php echo esc_url( site_url() ); ?>/games/save-the-pig"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/save-the-pig-featured.jpg" /></a>
     <div class="clear"></div>
   </div>
 
--- template-parts/footer-nav.php
+++ template-parts/footer-nav.php
@@ -6,9 +6,9 @@
 					<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
 					<li><a href="<?php echo esc_url( site_url() ); ?>/books">Books</a></li>
 					<li><a href="<?php echo esc_url( site_url() ); ?>/games">Games</a></li>
-					<li><a href="<?php echo esc_url( site_url() ); ?>/learning">Learning</a></li>
-					<li><a href="<?php echo esc_url( site_url() ); ?>/blog">Blog</a></li>
-					<li><a href="<?php echo esc_url( site_url() ); ?>/news">News</a></li>
+					<!-- <li><a href="<?php //echo esc_url( site_url() ); ?>/learning">Learning</a></li>
+					<li><a href="<?php //echo esc_url( site_url() ); ?>/blog">Blog</a></li>
+					<li><a href="<?php //echo esc_url( site_url() ); ?>/news">News</a></li> -->
 				</ul>
 				<div>
 					<a class="footer-mini-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/imgs/site/zimmy-books-logo-mini.png" alt="Zimmy Boooks! Logo" /></a>
